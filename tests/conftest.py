46e4cc6b2dd4baf4b4fadb7afbc6441f92a20a97
gonza <gajara@estudiantes.unsam.edu.ar>
2024-06-09 01:06:54 -0300

    deberia funcionar

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,11 @@
 import pytest
 from decision_trees.models.tree_models import DecisionTreeClassifier
 import pandas
-from sklearn.model_selection import train_test_split
 
 @pytest.fixture(scope="session")
-def decisionTreeNoParams():
+def overFittedTree():
     df = pandas.read_csv("play_tennis.csv")
     X = df.drop("play", axis=1)
     Y = df["play"]
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
     tree = DecisionTreeClassifier()
-    return tree.fit(X_train, Y_train)
-
-@pytest.fixture(scope="session")
-def decisionTreeMaxDepth4():
-    df = pandas.read_csv("play_tennis.csv")
-    X = df.drop("play", axis=1)
-    Y = df["play"]
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-    tree = DecisionTreeClassifier(max_depth=4)
-    return tree.fit(X_train, Y_train)
+    return tree.fit(X, Y)
--- tests/test_tree_models.py
+++ tests/test_tree_models.py
@@ -1,10 +1,11 @@
 import pytest
 import numpy as np
-import random
-
 # with pytest.raises(ValueError):
 #     some_function_raises_ValueError(*args)
 
-def test_baseTree(decisionTreeMaxDepth4):
-    assert decisionTreeMaxDepth4.get_depth() <= 4
+def test_baseTree(overFittedTree):
+    assert overFittedTree.get_depth() == 5
+
+if __name__ == "__main__":
+    test_baseTree()
     
