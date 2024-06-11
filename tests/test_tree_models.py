f76153afcda6922fd7084ae2ef7d4c32b6e8c60d
gonza <gajara@estudiantes.unsam.edu.ar>
2024-06-11 18:34:37 -0300

    primer_merge

--- tests/test_tree_models.py
+++ tests/test_tree_models.py
@@ -3,5 +3,20 @@ import numpy as np
 # with pytest.raises(ValueError):
 #     some_function_raises_ValueError(*args)
 
+def test_imports():
+    from decision_trees import BaseTree
+    from decision_trees import CategoricDecision
+    from decision_trees import RandomForestClassifier
+    from decision_trees import DecisionTreeClassifier
+
 def test_baseTree(overFittedTree):
+    assert overFittedTree.get_depth() == 5
+    assert overFittedTree.get_depth() == 5
+    assert overFittedTree.get_depth() == 5
+    assert overFittedTree.get_depth() == 5
+    assert overFittedTree.get_depth() == 5
+    assert overFittedTree.get_depth() == 5
+    assert overFittedTree.get_depth() == 5
+    assert overFittedTree.get_depth() == 5
+    assert overFittedTree.get_depth() == 5
     assert overFittedTree.get_depth() == 5
